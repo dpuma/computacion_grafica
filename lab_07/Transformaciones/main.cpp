b330b9e8f19f7422149831d79ef2a8b2f86706d9
dennis <dpumar3@gmail.com>
2022-06-14 17:30:18 -0500

    agregado mov balanceo

--- lab_07/Transformaciones/main.cpp
+++ lab_07/Transformaciones/main.cpp
@@ -9,6 +9,7 @@
 #include "shader_s.h"
 
 #include <iostream>
+using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -145,11 +146,13 @@ int main()
     ourShader.setInt("texture1", 0);
     ourShader.setInt("texture2", 1);
 
-
+    float mov;
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
+        mov = sin((float)glfwGetTime());
+
         // input
         // -----
         processInput(window);
@@ -165,9 +168,10 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
+        //cout << var2 << endl;
         // create transformations
         glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        transform = glm::translate(transform, glm::vec3(0.0f-mov, 0.0f+mov, 0.0f));
         transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 
         // get matrix's uniform location and set matrix
